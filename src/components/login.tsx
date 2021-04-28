1602d39f5515fb17bf68e4ff0a697d2273c36b6e
libiao <libiao@chinafocus.net>
2021-04-28 15:34:08 +0800

    页面调整

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './login.css';
-import logo from '../assets/logo.png';
+//import logo from '../assets/logo.png';
 import { connect } from 'dva';
 import { Layout, Form, Row, Col, Input, Button, Checkbox } from 'antd';
 const Item = Form.Item;
@@ -14,23 +14,23 @@ function Login(state: any) {
   const tailLayout = {
     wrapperCol: { offset: 8, span: 16 },
   };
-  function Login() { 
-    form.validateFields().then(values=>{ 
-      state.dispatch({type:"login/login",payload:{username:values.username,password:values.password}});
-    }).catch(errorInfo => {
-      console.log(errorInfo)
-    })
+  function Login() {
+    form
+      .validateFields()
+      .then((values) => {
+        state.dispatch({ type: 'login/login', payload: { username: values.username, password: values.password } });
+      })
+      .catch((errorInfo) => {
+        console.log(errorInfo);
+      });
   }
   return (
     <div className={styles.bg}>
       <div className={styles.center}>
         <Header className={styles.header}>
           <Row>
-            <Col span={11}>
-              <img src={logo} alt="" />{' '}
-            </Col>
-            <Col span={13}>
-              <p>管理中心</p>
+            <Col span={30}>
+              <p>人才需求信息检索系统</p>
             </Col>
           </Row>
         </Header>
@@ -39,23 +39,12 @@ function Login(state: any) {
             <Col className={styles.left}>
               <h3>账号登录</h3>
               <Form {...layout} form={form}>
-                <Item
-                  label="用户名"
-                  name="username"
-                  rules={[{ required: true, message: '请输入您的用户名' }]}
-                >
+                <Item label="用户名" name="username" rules={[{ required: true, message: '请输入您的用户名' }]}>
                   <Input autoComplete="new-password" />
                 </Item>
 
-                <Item
-                  label="密码"
-                  name="password"
-                  rules={[{ required: true, message: '请输入您的密码' }]}
-                >
-                  <Input.Password
-                    placeholder={'请输入密码'}
-                    autoComplete="new-password"
-                  />
+                <Item label="密码" name="password" rules={[{ required: true, message: '请输入您的密码' }]}>
+                  <Input.Password placeholder={'请输入密码'} autoComplete="new-password" />
                 </Item>
                 <Item {...tailLayout}>
                   <Button
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,10 +1,12 @@
-import {connect} from "dva"
-// import { Button } from 'antd';
-
+import { connect } from 'dva';
+import * as indexBj from '../../assets/bannerBJ.jpg';
 function IndexPage() {
   return (
-    <div>
-      <h1>首页</h1>
+    <div style={{ overflow: 'hidden', position: 'relative' }}>
+      <img src={indexBj} style={{ width: '100%' }} alt="" />
+      <div style={{ position: 'absolute', top: 60, margin: 'auto', height: '40px', width: '100%', textAlign: 'center', fontSize: '52px', color: '#fff' }}>
+        欢迎登陆人才需求信息检索系统
+      </div>
     </div>
   );
 }
@@ -13,8 +15,8 @@ IndexPage.propTypes = {
   // WebApi
 };
 
-function mapStateToProps(state:any) {
-   const { collapsed } = state.LeftMenu;
-   return {collapsed};
+function mapStateToProps(state: any) {
+  const { collapsed } = state.LeftMenu;
+  return { collapsed };
 }
 export default connect(mapStateToProps)(IndexPage);
